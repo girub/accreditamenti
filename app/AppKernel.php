81c5208920652e604dc3177004fdabe57e52a2e8
Giuseppe Rubino <giuseppe.rubino@gmail.com>
2012-05-30 11:58:00 +0200

    installato bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Accreditamenti\CongressiBundle\AccreditamentiCongressiBundle(),
+            new Ruian\TwitterBootstrapBundle\RuianTwitterBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- app/autoload.php
+++ app/autoload.php
@@ -23,6 +23,7 @@ $loader->registerNamespaces(array(
     'Doctrine\\DBAL\\Migrations'        => __DIR__.'/../vendor/doctrine-migrations/lib',
     'Sensio'                            => __DIR__.'/../vendor/bundles',
     'Symfony'                           => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Ruian' => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
@@ -51,4 +52,6 @@ AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/M
 // the lazy loading of the init file (which is expensive)
 require_once __DIR__.'/../vendor/swiftmailer/lib/classes/Swift.php';
 Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
+require  __DIR__.'/../vendor/lessphp/lessc.inc.php';
+
 
