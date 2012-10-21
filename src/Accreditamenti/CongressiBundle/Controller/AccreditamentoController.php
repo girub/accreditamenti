f7e046ed2a779a9aad9278de2339dece635a3e78
Simone Gentili <sensorario@gmail.com>
2012-10-21 10:51:03 +0200

    Domande e risposte ora appaiono in ordine casuale. [fixed #14]

--- src/Accreditamenti/CongressiBundle/Controller/AccreditamentoController.php
+++ src/Accreditamenti/CongressiBundle/Controller/AccreditamentoController.php
@@ -441,11 +441,14 @@ class AccreditamentoController extends Controller {
                 'multiple' => false,
                 'expanded' => true,
                 'query_builder' => function(RispostaRepository $risposta) use ($domanda) {
+                
                     $ordinamento = rand(0, 1) == 0 ? 'ASC' : 'DESC';
+                    
                     $elencoRisposte = $risposta->createQueryBuilder('r')
                             ->where('r.domanda=:domanda')
                             ->setParameter('domanda', $domanda)
                             ->orderBy('r.id', $ordinamento);
+                    
                     return $elencoRisposte;
                 },
             ));
