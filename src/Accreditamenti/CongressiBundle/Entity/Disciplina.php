a22aca97306a12d7c4b1e8764ca466428f21821e
Giuseppe Rubino <giuseppe.rubino@gmail.com>
2012-11-12 14:52:03 +0100

    create entiti professione e disciplina

---
+++ src/Accreditamenti/CongressiBundle/Entity/Disciplina.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Accreditamenti\CongressiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Accreditamenti\CongressiBundle\Entity\Disciplina
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Accreditamenti\CongressiBundle\Entity\DisciplinaRepository")
+ */
+class Disciplina
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer $codice
+     *
+     * @ORM\Column(name="codice", type="integer")
+     */
+    private $codice;
+
+    /**
+     * @var string $nome
+     *
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    private $nome;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set codice
+     *
+     * @param integer $codice
+     */
+    public function setCodice($codice)
+    {
+        $this->codice = $codice;
+    }
+
+    /**
+     * Get codice
+     *
+     * @return integer 
+     */
+    public function getCodice()
+    {
+        return $this->codice;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string 
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+}
