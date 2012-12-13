080a2d9c306208e9c2d1c9d3715df6141fce0279
Giuseppe Rubino <giuseppe.rubino@gmail.com>
2012-12-13 17:57:38 +0100

    aggiunti datapiker a pagina edit questinario ecm

--- src/Accreditamenti/CongressiBundle/Form/QuestionarioEcmType.php
+++ src/Accreditamenti/CongressiBundle/Form/QuestionarioEcmType.php
@@ -11,8 +11,18 @@ class QuestionarioEcmType extends AbstractType
     {
         $builder
             ->add('descrizione')
-            ->add('data_inizio_compilazione')
-            ->add('data_fine_compilazione')
+              ->add('data_inizio_compilazione', 'date', array(
+                    'label' => 'Data Inizio Compilazione',
+                    'widget' => 'single_text',
+                    'format' => 'dd-MM-yyyy')
+                )    
+               ->add('data_fine_compilazione', 'date', array(
+                    'label' => 'Data Fine Compilazione',
+                    'widget' => 'single_text',
+                    'format' => 'dd-MM-yyyy')
+                )    
+            //->add('data_inizio_compilazione')
+            //->add('data_fine_compilazione')
             ->add('percentuale_risposte_esatte')
             ->add('numero_tentativi_compilazione')
             ->add('accreditamento')
